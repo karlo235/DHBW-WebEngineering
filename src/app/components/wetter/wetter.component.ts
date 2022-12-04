91917066d465745188d4833f3c0e1f2ac5d370b1
Karlo Ivanovic <ivanovickarlo213@gmail.com>
2022-12-04 23:23:43 +0100

    Added comments in the weather component

--- src/app/components/wetter/wetter.component.ts
+++ src/app/components/wetter/wetter.component.ts
@@ -25,6 +25,7 @@ export class WetterComponent implements OnInit {
     });
   }
 
+  // Fetch data for current weather based on the location from the form
   getWeatherData(formValues: any): void {
     this.wetterService.getCurrentWeather(formValues.location).subscribe(
       (data: any) => {
@@ -35,11 +36,13 @@ export class WetterComponent implements OnInit {
     );
   }
 
+  // Fetch weather forecast data based on the location from the form
   getForecastData(formValues: any): void {
     this.wetterService.getForecast(formValues.location).subscribe(
       (data: any) => {
         console.log(data);
         this.forecastDetails.length = 0;
+        // Every 8th item because there is always data for 8 timepoints of a day
         for (var i = 7; i < data.list.length; i = i + 8) {
           this.forecastDetails.push(data.list[i]);
         }
@@ -48,18 +51,20 @@ export class WetterComponent implements OnInit {
     )
   }
 
+  // Format the date to be shown on the forecast card
   formatDate(date: string): string {
     const date1 = new Date(date);
     return date1.getDate() + '. ' + (date1.getMonth() + 1) + '. ' + date1.getFullYear();
   }
 
+  // Define which image will be used as background based on the weather data and current time
   defineBackgroundImage(): string {
     let backgroundImage = '../assets/background/';
     let hour = new Date().getHours();
-    let clear = ['Clear', 'Mist', 'Squall', 'Fog', 'Haze'];
-    let rain = ['Rain', 'Drizzle', 'Thunderstorm'];
+    let clear = ['Clear', 'Mist', 'Squall', 'Fog', 'Haze']; // If any of these conditions is met for a location, then a specific image will be used
+    let rain = ['Rain', 'Drizzle', 'Thunderstorm']; // If any of these conditions is met for a location, then a specific image will be used
     if (this.wetterDetails.main !== undefined) {
-      if (this.wetterDetails.weather[0].main === 'Clouds') {
+      if (this.wetterDetails.weather[0].main === 'Clouds') { // Single condition for a specific image
         backgroundImage += 'cloudy.jpg';
         return backgroundImage;
       }
@@ -71,14 +76,14 @@ export class WetterComponent implements OnInit {
       }
     }
     if (clear.indexOf(this.wetterDetails.weather[0].main) >= 0) {
-      if (this.wetterDetails.main.temp > 14) {
+      if (this.wetterDetails.main.temp > 14) { // Single condition for a specific temperature
         backgroundImage += 'warm.jpg';
       }
       else {
         backgroundImage += 'cold.jpg';
       }
     }
-    else if (this.wetterDetails.weather[0].main === 'Snow') {
+    else if (this.wetterDetails.weather[0].main === 'Snow') { // Single condition for a specific image
       backgroundImage += 'snow.jpg';
     }
     else if (rain.indexOf(this.wetterDetails.weather[0].main) >= 0) {
